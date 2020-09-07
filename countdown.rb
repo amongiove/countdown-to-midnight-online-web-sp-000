111238665467c0817205aa39293683e32fce59f6
Amanda Mongiove <amandamongiove@gmail.com>
2020-09-07 22:56:44 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,12 @@ def countdown(integer)
   end
   return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep
+  while integer > 0
+    puts "#{integer} SECOND(S)!"
+    sleep 2
+    integer -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
